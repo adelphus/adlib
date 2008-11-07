fe329cc978f328dd70178d8b293ff2e304c8573b
Ron Hopper <ron@adelphus.com>
2008-11-07 14:58:19 -0500

    Created controller for AdlibPages.

--- lib/adlib.rb
+++ lib/adlib.rb
@@ -4,6 +4,7 @@ Adlib::Migrator.migrate(ENV['VERSION'], false)
 
 require 'controllers/adlib_sessions_controller'
 require 'controllers/adlib_pages_controller'
+require 'controllers/adlib_snippets_controller'
 
 require 'models/adlib_user'
 require 'models/adlib_section'
