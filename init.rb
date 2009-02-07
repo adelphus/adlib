14fa3be6fd9bc6c98ebb4626977a3d78d02d1351
Ron Hopper <ron@adelphus.com>
2009-02-07 17:52:29 -0500

    Merged the BetterNestedSet plugin into adlib, so there is not a separate install required.

--- init.rb
+++ init.rb
@@ -1,1 +1,9 @@
+require 'vendor/plugins/betternestedset/lib/better_nested_set'
+require 'vendor/plugins/betternestedset/lib/better_nested_set_helper'
+
+ActiveRecord::Base.class_eval do
+  include SymetrieCom::Acts::NestedSet
+end
+ActionView::Base.send :include, SymetrieCom::Acts::BetterNestedSetHelper
+
 require 'adlib'
---
+++ test/rails_root/spec/helpers/adlib_toolbar_helpers_spec.rb
@@ -0,0 +1,73 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe AdlibHelper, "adlib_page_toolbar" do
+
+  describe "(not logged in)" do
+
+    before do
+      flexmock(self).should_receive(:adlib_logged_in?).and_return(false)
+    end
+    
+    it "should not display the toolbar" do
+      adlib_page_toolbar.should_not have_tag('div#adlib-page-toolbar')
+    end
+
+  end
+
+  describe "(logged in)" do
+
+    before do
+      flexmock(self).should_receive(:adlib_logged_in?).and_return(true)
+    end
+    
+    it "should display the toolbar" do
+      adlib_page_toolbar.should have_tag('div#adlib-page-toolbar')
+    end
+
+  end
+  
+end
+
+describe AdlibHelper, "powered_by_adlib" do
+
+  it "should display powered by adlib" do
+    powered_by_adlib.should have_tag('div#powered-by-adlib')
+  end
+
+  it "should display the link to adelphus" do
+    powered_by_adlib.should have_tag('a#link-to-adelphus')
+  end
+
+  describe "(not logged in)" do
+
+    before do
+      flexmock(self).should_receive(:adlib_logged_in?).and_return(false)
+    end
+
+    it "should display the link to log in" do
+      powered_by_adlib.should have_tag('a#link-to-adlib-session-new')
+    end
+
+    it "should not display the link to log out" do
+      powered_by_adlib.should_not have_tag('a#link-to-adlib-session-destroy')
+    end
+
+  end
+
+  describe "(logged in)" do
+
+    before do
+      flexmock(self).should_receive(:adlib_logged_in?).and_return(true)
+    end
+
+    it "should not display the link to log in" do
+      powered_by_adlib.should_not have_tag('a#link-to-adlib-session-new')
+    end
+
+    it "should display the link to log out" do
+      powered_by_adlib.should have_tag('a#link-to-adlib-session-destroy')
+    end
+
+  end
+
+end
